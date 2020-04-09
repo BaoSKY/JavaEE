e1cea6c662ccb68df6823e820b3791cdc21140b6
BaoSKY <1006710129@qq.com>
2020-04-09 09:50:11 +0800

    使用@Autowired获取bean

---
+++ springLoc/springMvc/src/main/java/com/model/Teacher.java
@@ -0,0 +1,44 @@
+package com.model;
+
+import org.springframework.context.annotation.Configuration;
+
+public class Teacher {
+
+    private int id;
+    private String name;
+    private String password;
+
+    public Teacher(){}
+
+    public int getId() {
+        return id;
+    }
+
+
+    public Teacher(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ springLoc/springMvc/src/main/java/com/util/Util.java
@@ -0,0 +1,13 @@
+package com.util;
+
+public class Util {
+
+    public static void main(String[] args) {
+        System.out.println("<p></p><p></p><form action=\"/assign\">\n" +
+                "    <label>作业ID：<input type=\"text\" name=\"hid\" value= readonly></label>\n" +
+                "    <label>作业标题：<input type=\"text\" name=\"hid\" value= readonly></label>\n" +
+                "    <label>添加学生：<input type=\"text\" name=\"students\"></label>\n" +
+                "    <input type=\"submit\" value=\"提交\">\n" +
+                "</form>");
+    }
+}
