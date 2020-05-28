11d233ed06f96319bdf840f3d8003881b5566de9
BaoSKY <1006710129@qq.com>
2020-05-28 09:47:10 +0800

    解决因为Spring boot和Spring cloud版本不一致导致的zuul和config无法启动的问题

--- SpringCloud/zuul/src/main/java/zuul/ApplicationZuul.java
+++ SpringCloud/zuul/src/main/java/zuul/ApplicationZuul.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
